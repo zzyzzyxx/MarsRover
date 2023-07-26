afb42d554677f5d21f243eb9c118b643e9214646
Sebastian Wrobel <64322145+zzyzzyxx@users.noreply.github.com>
2023-07-26 16:08:21 +0100

    Update PreferencesRepository.java

--- src/main/java/com/sebastianwrobel/repository/PreferencesRepository.java
+++ src/main/java/com/sebastianwrobel/repository/PreferencesRepository.java
@@ -11,5 +11,5 @@ public interface PreferencesRepository extends JpaRepository<HomeDto, Long>{
 //	@Query("select dto from HomeDto dto where userId = :userId")
 //	@Query(value="select * from mars_api_preferences where user_id = :userId", nativeQuery=true)
 	HomeDto findByUserId(Long userId);
-	
+	// repository revised
 }
